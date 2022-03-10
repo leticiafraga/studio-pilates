1748ce51f5ca78d1c7067c1e7b370ca563943541
leticiafraga <leticiafraga55@gmail.com>
2022-03-09 22:59:58 -0300

    add header.php

---
+++ header.php
@@ -0,0 +1,25 @@
+<header>
+    
+    <nav class="navbar navbar-expand-lg navbar-light">
+
+      <a class="navbar-brand" href="#"><img src="images/logo.png" alt="Studio Zen Pilates" class="logo"> </a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sobre nós</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
