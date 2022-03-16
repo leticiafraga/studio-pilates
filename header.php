892fe779ae700e7e4fb75d8322114edafa6bbb14
leticiafraga <leticiafraga55@gmail.com>
2022-03-16 20:55:47 -0300

    php header

---
+++ header.php
@@ -0,0 +1,24 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light">
+
+      <a class="navbar-brand" href="#"><img src="images/logo.png" alt="Studio Zen Pilates" class="logo"> </a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item <?php if ($page == 'home'){echo 'active'}?>">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="sobre.php">Sobre nós</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contato.php">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php $page='home'; include('header.php');?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -13,30 +15,7 @@
 </head>
 
 <body>
-  <header>
-    <nav class="navbar navbar-expand-lg navbar-light">
-
-      <a class="navbar-brand" href="#"><img src="images/logo.png" alt="Studio Zen Pilates" class="logo"> </a>
-
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="sobre.php">Sobre nós</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contato.php">Contato</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
+  
 
   <main class="container-fluid">
 
